fe9efbefa6ccee7ba5ea28cacc99ddfa913d481e
Luan Meira Quevedo <luan.quevedo@hotmail.com>
2024-09-19 15:48:53 -0300

    Corrigido erro de render da page services atualizado svg com erro

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './index.scss'
+import Footer from '../../components/footer/footer'
 
 const Home = () => {
   return (
-    <div>Teste</div>
+    <div><Footer/>
+    <img src="logoR.svg" alt="" srcset="" /></div>
   )
 }
 
