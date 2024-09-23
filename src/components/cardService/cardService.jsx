cd0c01a193dc90eb628c286c6653c63c898779d3
Luan Meira Quevedo <151397956+Luanquevedo@users.noreply.github.com>
2024-09-23 16:45:09 -0300

    Update cardService.jsx
    Att cardService

--- src/components/cardService/cardService.jsx
+++ src/components/cardService/cardService.jsx
@@ -6,12 +6,10 @@ const CardService = ({img, title, descr, text}) => {
     <div className="card_service">
       <div className="img_card">
         <img src={img} alt={title} />
-        <div className="title_card">
           <h1>{title}</h1>
-          <p>{descr}</p>
-        </div>
       </div>
-      <p>{text}</p>
+      <p>{descr}</p>  
+      <p className='text_card'>{text}</p>
     </div>
   )
 }
